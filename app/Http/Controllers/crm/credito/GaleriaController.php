f4025af40b3cf2a24bf20d0e4c4d06ce12fb3798
Almacenes España <114423389+sistemascuencaae@users.noreply.github.com>
2023-06-02 18:48:20 -0500

    Token galeriaController

--- app/Http/Controllers/crm/credito/GaleriaController.php
+++ app/Http/Controllers/crm/credito/GaleriaController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Storage;
 
 class GaleriaController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     public function store(Request $request)
     {
