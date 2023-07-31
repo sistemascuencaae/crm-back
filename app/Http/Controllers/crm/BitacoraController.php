2ff739aaa8114a0327d40cb55131c011225220f8
Juan Simbaña <84163387+JuanJsj1@users.noreply.github.com>
2023-07-31 11:00:58 -0500

    fix code version

--- app/Http/Controllers/crm/BitacoraController.php
+++ app/Http/Controllers/crm/BitacoraController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\crm;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RespuestaApi;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,8 +13,8 @@ class BitacoraController extends Controller
     public function listBitacoraByCasoId($caso_id)
     {
         // $bitacora = DB::select('select * from public.audits');
-
-        $bitacora = DB::select("select adi.*,ur.name,gal.titulo  from public.audits adi
+        try {
+            $bitacora = DB::select("select adi.*,ur.name,gal.titulo  from public.audits adi
         left join crm.archivos arc on arc.id = adi.auditable_id and adi.auditable_type = 'App\Models\crm\Archivo'
         left join crm.galerias gal on gal.id = adi.auditable_id and adi.auditable_type = 'App\Models\crm\Galeria'
         left join crm.comentarios c on c.id = adi.auditable_id and adi.auditable_type = 'App\Models\crm\Comentarios'
@@ -23,8 +25,12 @@ class BitacoraController extends Controller
         where cas.id = " . $caso_id . "
         order By 1 DESC");
 
-        return response()->json([
-            "bitacora" => $bitacora,
-        ]);
+            // return response()->json([
+            //     "bitacora" => $bitacora,
+            // ]);
+            return response()->json(RespuestaApi::returnResultado('success', 'Se listo con éxito', $bitacora));
+        } catch (Exception $e) {
+            return response()->json(RespuestaApi::returnResultado('error', 'Error', $e));
+        }
     }
 }
