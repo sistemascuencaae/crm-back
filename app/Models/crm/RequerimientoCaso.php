59f0a627357baac9d3b89d302f2ad487526b56ca
Tomas Felipe <97459312+felipeAlmEspa@users.noreply.github.com>
2023-08-20 20:30:56 -0500

    comit req caso

---
+++ app/Models/crm/RequerimientoCaso.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\crm;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RequerimientoCaso extends Model
+{
+    use HasFactory;
+    protected $table = 'crm.requerimientos_caso';
+
+
+    protected $fillable = [
+       "id",
+       "descripcion",
+       "caso_id",
+       "created_at",
+       "updated_at",
+       "deleted_at",
+       "marcado",
+       "estado",
+       "tipo_req_id",
+       "user_requiere_id",
+       "titulo",
+       "tipo_campo",
+       "requerido",
+       "valor_date",
+       "valor_int",
+       "valor_boolean",
+       "valor_varchar",
+       "valor_decimal",
+       "html_render",
+       "valor",
+       "form_control_name",
+       "valor_multiple",
+       "orden",
+    ];
+}
