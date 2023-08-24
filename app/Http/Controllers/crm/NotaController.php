985f59f686b5bf1dedf3c7f0b810aefc6aed7389
Juan Simbaña <84163387+JuanJsj1@users.noreply.github.com>
2023-08-24 12:55:09 -0500

    Audits Notas

--- app/Http/Controllers/crm/NotaController.php
+++ app/Http/Controllers/crm/NotaController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\crm;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RespuestaApi;
+use App\Models\crm\Audits;
 use App\Models\crm\Nota;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class NotaController extends Controller
@@ -20,6 +23,23 @@ class NotaController extends Controller
     {
         try {
             $nota = Nota::create($request->all());
+
+            // START Bloque de código que genera un registro de auditoría manualmente
+            $audit = new Audits();
+            $audit->user_id = Auth::id();
+            $audit->event = 'created';
+            $audit->auditable_type = Nota::class;
+            $audit->auditable_id = $nota->id;
+            $audit->user_type = User::class;
+            $audit->ip_address = $request->ip(); // Obtener la dirección IP del cliente
+            $audit->url = $request->fullUrl();
+            // Establecer old_values y new_values
+            $audit->old_values = json_encode($nota);
+            $audit->new_values = json_encode([]);
+            $audit->user_agent = $request->header('User-Agent'); // Obtener el valor del User-Agent
+            $audit->accion = 'addNota';
+            $audit->save();
+            // END Auditoria
 
             $data = DB::select('select * from crm.nota where caso_id =' . $request->caso_id);
 
@@ -45,11 +65,31 @@ class NotaController extends Controller
         try {
             $nota = Nota::findOrFail($id);
 
+            // Obtener el old_values (valor antiguo)
+            $audit = new Audits();
+            $valorAntiguo = $nota;
+            $audit->old_values = json_encode($valorAntiguo);
+
             // $nota->update([
             //     "nombre" => $request->nombre,
             // ]);
 
             $nota->update($request->all());
+
+            // START Bloque de código que genera un registro de auditoría manualmente
+            $audit->user_id = Auth::id();
+            $audit->event = 'updated';
+            $audit->auditable_type = Nota::class;
+            $audit->auditable_id = $nota->id;
+            $audit->user_type = User::class;
+            $audit->ip_address = $request->ip(); // Obtener la dirección IP del cliente
+            $audit->url = $request->fullUrl();
+            // Establecer old_values y new_values
+            $audit->new_values = json_encode($nota);
+            $audit->user_agent = $request->header('User-Agent'); // Obtener el valor del User-Agent
+            $audit->accion = 'editNota';
+            $audit->save();
+            // END Auditoria
 
             return response()->json(RespuestaApi::returnResultado('success', 'Se actualizo con éxito', $nota));
         } catch (Exception $e) {
@@ -57,12 +97,31 @@ class NotaController extends Controller
         }
     }
 
-    public function deleteNota($id)
+    public function deleteNota(Request $request, $id)
     {
         try {
             $nota = Nota::findOrFail($id);
+            // Obtener el old_values (valor antiguo)
+            $valorAntiguo = $nota;
 
             $nota->delete();
+
+            // START Bloque de código que genera un registro de auditoría manualmente
+            $audit = new Audits();
+            $audit->user_id = Auth::id();
+            $audit->event = 'deleted';
+            $audit->auditable_type = Nota::class;
+            $audit->auditable_id = $nota->id;
+            $audit->user_type = User::class;
+            $audit->ip_address = $request->ip(); // Obtener la dirección IP del cliente
+            $audit->url = $request->fullUrl();
+            // Establecer old_values y new_values
+            $audit->old_values = json_encode($valorAntiguo);
+            $audit->new_values = json_encode([]);
+            $audit->user_agent = $request->header('User-Agent'); // Obtener el valor del User-Agent
+            $audit->accion = 'deleteNota';
+            $audit->save();
+            // END Auditoria
 
             return response()->json(RespuestaApi::returnResultado('success', 'Se elimino con éxito', $nota));
         } catch (Exception $e) {
