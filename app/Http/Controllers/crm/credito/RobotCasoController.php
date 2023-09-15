deeaa6465e131448a758330a0fc5d866b736166c
Tomas Felipe <97459312+felipeAlmEspa@users.noreply.github.com>
2023-09-15 10:52:43 -0500

    commit termporal reasignacion usuario general

--- app/Http/Controllers/crm/credito/RobotCasoController.php
+++ app/Http/Controllers/crm/credito/RobotCasoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\crm\credito;
 
+use App\Events\ReasignarCasoEvent;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\crm\CasoController;
 use App\Http\Resources\RespuestaApi;
 use App\Models\crm\Caso;
 use App\Models\crm\EstadosFormulas;
@@ -15,40 +17,39 @@ use Illuminate\Support\Facades\DB;
 
 class RobotCasoController extends Controller
 {
+
+
+
+
     public function reasignarCaso(Request $request)
     {
 
 
-        //try {
+        try {
 
-        $estadoFormId = $request->input('estadoFormId');
-        $casoId = $request->input('casoId');
-
-
-        $casoEnProceso = Caso::find($casoId);
-        //$casoModificado = $this->validacionReasignacionUsuario($estadoFormId, $casoId);
-        // return response()->json([
-        //     "antes" => $casoEnProceso,
-        //     "despues" => $casoModificado
-        // ]);
-
-        return $this->validacionReasignacionUsuario($estadoFormId, $casoId);
-
-
-        //----------------------------------------------------------------FALATA EL NUEVO USUARIO ASIGNADO
-
-
-        //$casoEnProceso->save();
-        //$data = $this->getCaso($casoEnProceso->id);
-        //broadcast(new ReasignarCasoEvent($data));
-        // return response()->json(RespuestaApi::returnResultado('success', 'Se guardo con éxito', $dataInput));
-        // } catch (Exception $e) {
-        //     return response()->json(RespuestaApi::returnResultado('error', 'Error', $e));
-        // }
-
+            $casoController = new CasoController();
+            $estadoFormId = $request->input('estadoFormId');
+            $casoId = $request->input('casoId');
+            $tableroActualId = $request->input('tableroActualId');
+            $facturaId = $request->input('facturaId');
+            $casoEnProceso = Caso::find($casoId);
+            $casoModificado = $this->validacionReasignacionUsuario($estadoFormId, $casoId, $tableroActualId, $facturaId);
+            // return response()->json([
+            //     "antes" => $casoEnProceso,
+            //     "despues" => $casoModificado
+            // ]);
+            // return $this->validacionReasignacionUsuario($estadoFormId, $casoId, $tableroActualId, $facturaId);
+            //----------------------------------------------------------------FALATA EL NUEVO USUARIO ASIGNADO
+            //$casoEnProceso->save();
+            $data = $casoController->getCaso($casoModificado->id);
+            broadcast(new ReasignarCasoEvent($data));
+            return response()->json(RespuestaApi::returnResultado('success', 'Reasignado con exito', $data));
+        } catch (Exception $e) {
+            return response()->json(RespuestaApi::returnResultado('error', 'Error al reasignar', $e));
+        }
     }
 
-    private function validacionReasignacionUsuario($estadoFormId, $casoId)
+    private function validacionReasignacionUsuario($estadoFormId, $casoId, $tableroActualId, $facturaId)
     {
 
         '"antes": {
@@ -100,36 +101,92 @@ class RobotCasoController extends Controller
                 $usuGeneral = DB::selectOne('SELECT * FROM public.users where tab_id = ? and usu_tipo = 1;', [$formula->tablero_id]);
                 $casoEnProceso->user_id = $usuGeneral->id;
             }
-            //$casoEnProceso->save();
+            $casoEnProceso->save();
             return $casoEnProceso;
         }
+
+
+        $temp = '{
+	{
+		"id" : 2,
+		"name" : "MARIUXI LEMA",
+		"usu_tipo_analista" : 1,
+		"monto_inicial" : 0,
+		"monto_limite" : 0
+	},
+	{
+		"id" : 2068,
+		"name" : "Credito 3",
+		"usu_tipo_analista" : 3,
+		"monto_inicial" : 1,
+		"monto_limite" : 3500
+	},
+	{
+		"id" : 2116,
+		"name" : "USUARIO GENERAL TEST CREDITO 3 159",
+		"usu_tipo_analista" : 1,
+		"monto_inicial" : 0,
+		"monto_limite" : 0
+	}
+    ]}
+    ';
+        //---Usuarios en linea del nuevo tablero y sus perfiles
+        $usuariosNuevoTablero = DB::select("SELECT usu.id as usu_id,usu.usu_tipo, usu.name, usu.usu_tipo_analista, pa.nombre, pa.monto_inicial, pa.monto_limite from crm.tablero tab
+        inner join crm.tablero_user tu on tu.tab_id = tab.id
+        inner join public.users usu on usu.id = tu.user_id
+        inner join crm.perfil_analistas pa on pa.id = usu.usu_tipo_analista
+        where tab.id = ? and usu.estado = true and usu.en_linea = true", [$formula->tablero_id]);
+
+        //---Temporalmente enviar al usuario general
+        foreach ($usuariosNuevoTablero as $key => $value) {
+            if ($value->usu_tipo == 1) {
+                $casoEnProceso->user_id = $value->usu_id;
+                $casoEnProceso->save();
+                return $casoEnProceso;
+                break;
+            }
+        }
+
+        return $casoEnProceso;
+
         //------------------------------------------------------------
         //-------------------OPCION 2---------------------------------
         //------------------------------------------------------------
-        //echo ('test: 2 -> no es el tablero de creacion');
-        //--- Buscar usuarios del nuevo tablero
-        $newTablero = Tablero::find($formula->tablero_id)->with('tableroUsuario.usuario.perfil_analista')->firstOrFail();
-        $newTableroToString = json_encode($newTablero);
-        $tablero = json_decode($newTableroToString);
-        $usuariosNuevoTablero = $tablero->tablero_usuario;
-
-        foreach ($usuariosNuevoTablero as $tu) {
-            //--usuarios en lienea
-            if($tu->usuario->en_linea){
-
-
-
-
-
-
-
-
-
-
-
-                echo ('$tu->usuario: '.json_encode($tu->usuario));
+        //---Si el nuevo tablero NO ES el tablero de creacio buscar en usuario anterior agrega solo si el usuario es el anterior
+        if ($formula->tablero_id != $casoEnProceso->tablero_creacion_id) {
+            foreach ($usuariosNuevoTablero as $key => $value) {
+                if ($value->usu_id == $casoEnProceso->user_anterior_id) {
+                    $casoEnProceso->user_id = $casoEnProceso->user_creador_id;
+                    //$casoEnProceso->save();
+                    return $casoEnProceso;
+                }
             }
         }
+        //------------------------------------------------------------
+        //-------------------OPCION 3---------------------------------
+        //------------------------------------------------------------
+        //echo ('test: 2 -> no es el tablero de creacion');
+        //--- Buscar usuarios del nuevo tablero
+        // $newTablero = Tablero::find($formula->tablero_id)->with('tableroUsuario.usuario.perfil_analista')->firstOrFail();
+        // $newTableroToString = json_encode($newTablero);
+        // $tablero = json_decode($newTableroToString);
+        // $usuariosNuevoTablero = $tablero->tablero_usuario;
+
+        //---Analizar perfil de analista
+        //---Si ya tenemos la factura o pedido
+        $userCumplenPerfil = null;
+        $facturaId != null ?? $userCumplenPerfil = $this->analizarPerfilAnalista($usuariosNuevoTablero);
+
+
+
+
+
+
+
+
+
+
+
 
 
         //--tengo todos los usuarios que estan en linea del nuevo tablero
@@ -140,5 +197,23 @@ class RobotCasoController extends Controller
 
 
         //--- retorna opcion 1 hasta el momento
+    }
+
+    private function analizarPerfilAnalista($usuarios)
+    {
+    }
+
+    private function analizarCasosPorUsario($usuarios)
+    {
+        $casosUsuario = new Collection();
+
+        //2068
+        //2067
+        //2066
+
+
+
+
+
     }
 }
