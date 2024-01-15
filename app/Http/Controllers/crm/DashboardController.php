e7984e935c648d6050562dec8f4e107193f2fcfa
Juan Simbaña <84163387+JuanJsj1@users.noreply.github.com>
2024-01-15 10:16:24 -0500

    logs en DepartamentoController y DashboardController

--- app/Http/Controllers/crm/DashboardController.php
+++ app/Http/Controllers/crm/DashboardController.php
@@ -3,20 +3,26 @@
 namespace App\Http\Controllers\crm;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\crm\Funciones;
 use App\Http\Resources\RespuestaApi;
 use App\Models\views\ProductoClienteView;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
     public function comprasCliente($entId)
     {
+        $log = new Funciones();
         try {
             $data = ProductoClienteView::where('ent_id', $entId)->where('devuelto', false)->get();
+
+            $log->logInfo(DashboardController::class, 'Se listo con exito las compras del cliente');
+
             return response()->json(RespuestaApi::returnResultado('success', 'Listado con exito', $data));
-        } catch (\Throwable $th) {
-            return response()->json(RespuestaApi::returnResultado('exception', 'Al listar', $th->getMessage()));
+        } catch (\Throwable $e) {
+            $log->logError(DashboardController::class, 'Error al listar las compras del cliente', $e);
+
+            return response()->json(RespuestaApi::returnResultado('exception', 'Al listar', $e->getMessage()));
         }
     }
 }
