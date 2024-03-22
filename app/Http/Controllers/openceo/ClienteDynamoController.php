c280193b7aeb3fa071d753e1411ce879c0a4ca45
Juan Simbaña <84163387+JuanJsj1@users.noreply.github.com>
2024-03-22 16:43:39 -0500

    fix

--- app/Http/Controllers/openceo/ClienteDynamoController.php
+++ app/Http/Controllers/openceo/ClienteDynamoController.php
@@ -24,7 +24,7 @@ class ClienteDynamoController extends Controller
             'direccion' => 'required|string',
             'telefono' => 'required|string',
             'identificacion' => 'required|string',
-            'tipoIdentificacion' => 'required|string',
+            'tipoidentificacion' => 'required|string',
             'nombres' => 'required|string',
             'apellidos' => 'required|string',
             'email' => 'required|string',
@@ -37,75 +37,90 @@ class ClienteDynamoController extends Controller
             return response()->json(RespuestaApi::returnResultado('error', 'Validación datos', $validator->errors()));
         }
         try {
-            $cliente = DB::transaction(function () use ($request) {
-                $direccion = $request->input('direccion');
-                $telefono = $request->input('telefono');
-                $identificacion = $request->input('identificacion');
-                $tipoIdentificacion = $request->input('tipoIdentificacion');
-                $nombres = $request->input('nombres');
-                $apellidos = $request->input('apellidos');
-                $email = $request->input('email');
-                $empId = $request->input('empId');
-                $identificacionConyugue = $request->input('identificacionConyugue');
-                $nombreConyugue = $request->input('nombreConyugue');
-                $apellidoConyugue = $request->input('apellidoConyugue');
-                //--- NUEVA DIRECCION
-                $newDireccion = new Direccion();
-                $newDireccion->dir_calle_principal = $direccion;
-                $newDireccion->save();
-                //--- NUEVO TELEFONO
-                $newTelefono = new Telefono();
-                $newTelefono->tte_id = 1;
-                $newTelefono->tel_numero = $telefono;
-                $newTelefono->save();
-                //--- NUEVA ENTIDAD
-                $valor1 = DB::selectOne("SELECT to_number(par_texto,'999999') as tit_id from parametro where par_abreviacion='TIT' and mod_abreviatura='CLI' limit 1");
-                $newEntidad = new Entidad();
-                $newEntidad->ent_identificacion = $identificacion;
-                $newEntidad->ent_nombres = $nombres;
-                $newEntidad->ent_apellidos = $apellidos;
-                $newEntidad->tit_id = $valor1->tit_id;
-                $newEntidad->ent_direccion_principal = $newDireccion->dir_id;
-                $newEntidad->ent_tipo_identificacion = $tipoIdentificacion;
-                $newEntidad->ent_email = $email;
-                $newEntidad->ent_telefono_principal = $newTelefono->tel_id;
-                $newEntidad->save();
-                //--- NUEVO CLIENTE
-                $valor2 = DB::selectOne("SELECT to_number(par_texto,'999999') as tit_id from parametro where par_abreviacion='UBI' and mod_abreviatura='CLI' LIMIT 1");
-                $valor3 = DB::selectOne("SELECT zon_id from zona where zon_codigo in (select par_texto from parametro
+
+
+            $entidad = DB::selectOne("SELECT 
+            *
+            FROM public.entidad e 
+            left join public.cliente c on c.ent_id = e.ent_id 
+            WHERE e.ent_identificacion = ?", [$request->input('identificacion')]);
+
+            if (!$entidad) {
+
+                $cliente = DB::transaction(function () use ($request) {
+                    $direccion = $request->input('direccion');
+                    $telefono = $request->input('telefono');
+                    $identificacion = $request->input('identificacion');
+                    $tipoIdentificacion = $request->input('tipoidentificacion');
+                    $nombres = $request->input('nombres');
+                    $apellidos = $request->input('apellidos');
+                    $email = $request->input('email');
+                    $empId = $request->input('empId');
+                    $identificacionConyugue = $request->input('identificacionConyugue');
+                    $nombreConyugue = $request->input('nombreConyugue');
+                    $apellidoConyugue = $request->input('apellidoConyugue');
+                    //--- NUEVA DIRECCION
+                    $newDireccion = new Direccion();
+                    $newDireccion->dir_calle_principal = $direccion;
+                    $newDireccion->save();
+                    //--- NUEVO TELEFONO
+                    $newTelefono = new Telefono();
+                    $newTelefono->tte_id = 1;
+                    $newTelefono->tel_numero = $telefono;
+                    $newTelefono->save();
+                    //--- NUEVA ENTIDAD
+                    $valor1 = DB::selectOne("SELECT to_number(par_texto,'999999') as tit_id from parametro where par_abreviacion='TIT' and mod_abreviatura='CLI' limit 1");
+                    $newEntidad = new Entidad();
+                    $newEntidad->ent_identificacion = $identificacion;
+                    $newEntidad->ent_nombres = $nombres;
+                    $newEntidad->ent_apellidos = $apellidos;
+                    $newEntidad->tit_id = $valor1->tit_id;
+                    $newEntidad->ent_direccion_principal = $newDireccion->dir_id;
+                    $newEntidad->ent_tipo_identificacion = $tipoIdentificacion;
+                    $newEntidad->ent_email = $email;
+                    $newEntidad->ent_telefono_principal = $newTelefono->tel_id;
+                    $newEntidad->save();
+                    //--- NUEVO CLIENTE
+                    $valor2 = DB::selectOne("SELECT to_number(par_texto,'999999') as tit_id from parametro where par_abreviacion='UBI' and mod_abreviatura='CLI' LIMIT 1");
+                    $valor3 = DB::selectOne("SELECT zon_id from zona where zon_codigo in (select par_texto from parametro
                             where par_abreviacion='ZON' and mod_abreviatura='CLI' limit 1) limit 1");
-                $valor4 = DB::selectOne("SELECT cat_id from catcliente where cat_abreviacion = 'clien'");
-                $valor5 = DB::selectOne("SELECT pol_id from politica where pol_nombre = 'CONTADO' and pol_tipocli = 1");
-                $valor6 = DB::selectOne("SELECT lpr_id from listapre where lpr_nombre in (select par_texto
+                    $valor4 = DB::selectOne("SELECT cat_id from catcliente where cat_abreviacion = 'clien'");
+                    $valor5 = DB::selectOne("SELECT pol_id from politica where pol_nombre = 'CONTADO' and pol_tipocli = 1");
+                    $valor6 = DB::selectOne("SELECT lpr_id from listapre where lpr_nombre in (select par_texto
                             from parametro where par_abreviacion='LPR' and mod_abreviatura='CLI' limit 1) limit 1");
-                $valor7 = DB::selectOne("SELECT to_number(par_texto,'999999') as can_id from parametro where par_abreviacion='CAN' and mod_abreviatura='CLI' limit 1");
-                $newCliente = new Cliente();
-                $newCliente->cli_codigo = $identificacion;
-                $newCliente->ent_id = $newEntidad->ent_id;
-                $newCliente->ubi_id = $valor2->tit_id;
-                $newCliente->zon_id = $valor3->zon_id;
-                $newCliente->cat_id = $valor4->cat_id;
-                $newCliente->pol_id = $valor5->pol_id;
-                $newCliente->lpr_id = $valor6->lpr_id;
-                $newCliente->cli_tipocli = 1;
-                $newCliente->emp_id = $empId;
-                $newCliente->can_id = $valor7->can_id;
-                $newCliente->ent_nombre_comercial = $nombres . ' ' . $apellidos;
-                $newCliente->cli_tiposujeto = 'N';
-                $newCliente->cli_sexo = 'M';
-                $newCliente->cli_estadocivil = 'S';
-                $newCliente->cli_ingresos = 'I';
-                $newCliente->save();
-                $cliTipoPago = DB::insert("insert into cliente_tipo_pago(cli_id, sfp_id) values (?, 1)", [$newCliente->cli_id]);
-                if ($identificacionConyugue && $nombreConyugue && $apellidoConyugue) {
-                    $cliAnexo = DB::insert(
-                        "INSERT into cliente_anexo(cliane_identificacion_conyuge, cliane_nombre_conyuge,cli_id) values (?,?,?)",
-                        [$identificacionConyugue, $nombreConyugue, $newCliente->cli_id]
-                    );
-                }
-                return $newCliente;
-            });
-            return response()->json(RespuestaApi::returnResultado('success', 'Listado con exito', $cliente));
+                    $valor7 = DB::selectOne("SELECT to_number(par_texto,'999999') as can_id from parametro where par_abreviacion='CAN' and mod_abreviatura='CLI' limit 1");
+                    $newCliente = new Cliente();
+                    $newCliente->cli_codigo = $identificacion;
+                    $newCliente->ent_id = $newEntidad->ent_id;
+                    $newCliente->ubi_id = $valor2->tit_id;
+                    $newCliente->zon_id = $valor3->zon_id;
+                    $newCliente->cat_id = $valor4->cat_id;
+                    $newCliente->pol_id = $valor5->pol_id;
+                    $newCliente->lpr_id = $valor6->lpr_id;
+                    $newCliente->cli_tipocli = 1;
+                    $newCliente->emp_id = $empId;
+                    $newCliente->can_id = $valor7->can_id;
+                    $newCliente->ent_nombre_comercial = $nombres . ' ' . $apellidos;
+                    $newCliente->cli_tiposujeto = 'N';
+                    $newCliente->cli_sexo = 'M';
+                    $newCliente->cli_estadocivil = 'S';
+                    $newCliente->cli_ingresos = 'I';
+                    $newCliente->save();
+                    $cliTipoPago = DB::insert("insert into cliente_tipo_pago(cli_id, sfp_id) values (?, 1)", [$newCliente->cli_id]);
+                    if ($identificacionConyugue && $nombreConyugue && $apellidoConyugue) {
+                        $cliAnexo = DB::insert(
+                            "INSERT into cliente_anexo(cliane_identificacion_conyuge, cliane_nombre_conyuge,cli_id) values (?,?,?)",
+                            [$identificacionConyugue, $nombreConyugue, $newCliente->cli_id]
+                        );
+                    }
+                    return $newCliente;
+                });
+
+                return response()->json(RespuestaApi::returnResultado('success', 'Listado con exito', $cliente));
+            } else {
+                return response()->json(RespuestaApi::returnResultado('success', 'Listado con exito', $entidad));
+            }
+
         } catch (\Throwable $th) {
             return response()->json(RespuestaApi::returnResultado('exception', 'Al listar', $th->getMessage()));
         }
